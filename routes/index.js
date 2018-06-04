b4942af71eba57ca2486db26dfdfbbdc60e245cb
truongdx <truongdx@beetsoft.com.vn>
2018-06-04 17:37:44 +0700

    NodeJs Bai3 chuong 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+
+router.get('/dashboards', function(req, res, next) {
+  res.render('page/dashboard/index', { title: 'DashBoards Page' });
 });
 
 module.exports = router;
---
+++ routes/items.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET items listing. */
+router.get('/list', function(req, res, next) {
+  res.render('page/items/item-list', { title: 'Item List Page' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('page/items/item-add', { title: 'Add Item Page' });
+});
+
+module.exports = router;
