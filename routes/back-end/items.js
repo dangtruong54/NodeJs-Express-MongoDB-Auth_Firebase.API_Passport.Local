9803d9d181206067d72f808166195fd8fbb8399b
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-06-25 08:27:11 +0700

    Phuong thuc update 1 item voi mongoose

--- routes/back-end/items.js
+++ routes/back-end/items.js
@@ -6,25 +6,25 @@ const UtilsHelper = require('./../../helper/utils');
 /* GET items listing. */
 router.get('(/:status)?', (req, res, next) => {
 	let requestStatus = "";
-	 	requestStatus = req.params.status;
-		requestStatus = (requestStatus == undefined) ? 'all' : requestStatus;
+	requestStatus = req.params.status;
+	requestStatus = (requestStatus == undefined) ? 'all' : requestStatus;
 
 	let requestQuery = '';
 	requestQuery = req.query.keyword ? req.query.keyword : requestQuery;
 
 	let objWhere = {};
 
-	if(requestStatus != 'all') {
+	if (requestStatus != 'all') {
 		objWhere = {
 			status: requestStatus,
-			name : new RegExp(requestQuery, 'i')
+			name: new RegExp(requestQuery, 'i')
 		}
 	} else {
-		objWhere = {name : new RegExp(requestQuery, 'i')};
+		objWhere = { name: new RegExp(requestQuery, 'i') };
 	};
-	
+
 	let currentPage = 1;
-	currentPage =  parseInt(req.query.page) ? parseInt(req.query.page) : currentPage;
+	currentPage = parseInt(req.query.page) ? parseInt(req.query.page) : currentPage;
 
 	let totalItem = 1;
 
@@ -32,41 +32,57 @@ router.get('(/:status)?', (req, res, next) => {
 		totalItem,
 		currentPage,
 		itemPerPage: 1,
-		pageRanges: 3	
+		pageRanges: 3
 	}
 	let statusActive = [];
 
-	ItemsModel.find({}).then(function(items) {
+	ItemsModel.find({}).then(function (items) {
 		statusActive = UtilsHelper.statusHelper(items, requestStatus);
-	})		
-	
-	ItemsModel.count(objWhere).then(function(items) {
+	})
+
+	ItemsModel.count(objWhere).then(function (items) {
 		paramsPagination.totalItem = items;
 		ItemsModel
-		.find(objWhere)
-		.sort({ordering: 'asc'})
-		.limit(paramsPagination.itemPerPage)
-		.skip((paramsPagination.currentPage - 1) * paramsPagination.itemPerPage)
-		.then( (items) => {			
-			res.render(
-				'page/items/item-list',
-				{ 
-					title: 'Item List Page',
-					items,
-					statusActive,
-					requestStatus,
-					requestQuery,
-					paramsPagination									
-				}
-			);   
-		})
+			.find(objWhere)
+			.sort({ ordering: 'asc' })
+			.limit(paramsPagination.itemPerPage)
+			.skip((paramsPagination.currentPage - 1) * paramsPagination.itemPerPage)
+			.then((items) => {
+				res.render(
+					'page/items/item-list',
+					{
+						title: 'Item List Page',
+						items,
+						statusActive,
+						requestStatus,
+						requestQuery,
+						paramsPagination
+					}
+				);
+			})
 	})
 });
 
 router.get('/change-status/:id/:status', function (req, res, next) {
 	let id = req.params.id ? req.params.id : '';
 	let currentStatus = req.params.status ? req.params.status : '';
-	res.send(id + '-' + currentStatus);
+
+	let status = (currentStatus === 'active') ? 'inactive' : 'active';
+
+	// ItemsModel.update({ _id: id }, { status:  status}, (err, affect, resp) => {
+	// 	console.log(affect);
+	// });
+
+	// ItemsModel.findById(id).then( (item) => {
+	// 	item.status = status;
+	// 	item.save().then( (result) => {
+	// 		res.redirect('/admin/items/');
+	// 	});
+	// });
+
+	ItemsModel.updateOne({ _id: id }, { status: status }, function (err, result) {
+		res.redirect('/admin/items/');
+	});
 });
 
 router.get('/add', function (req, res, next) {
--- schemas/items.js
+++ schemas/items.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate');
 
 var schema = new mongoose.Schema({ 
         name: String,
         status: String,
         ordering: Number
     });
-
-schema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('items', schema);
