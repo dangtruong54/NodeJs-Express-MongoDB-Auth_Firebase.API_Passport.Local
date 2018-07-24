c4f27eac05ef0cf4269c69b7e787df856c54b117
truongdx <truongdx@beetsoft.com.vn>
2018-07-24 17:24:59 +0700

    notify2 ba14 chuong 3

--- helper/notify.js
+++ helper/notify.js
@@ -5,5 +5,5 @@ module.exports = {
     UPDATE_ITEM: "Cập nhật thông tin item thành công",
     ADD_ITEM: "Thêm item thành công",
     DELETE_ITEM: "Xóa item thành công",
-    DELETE_MULTIL_ITEM: "Xóa nhiều item thành công",    
+    DELETE_MULTIL_ITEM: "Xóa %s item thành công",    
 }
